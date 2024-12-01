9523239e8c3311b25b4ea0d4196b3a9a143ff59c
Raj-6250 <rajchaurasia625@gmail.com>
2024-12-02 04:51:35 +0530

    clock-project

---
+++ script.js
@@ -0,0 +1,11 @@
+setInterval(() => {
+    const element =document.querySelector('.clock')
+const time=new Date();
+const option={
+    timeZone:'Asia/Kolkata',
+    hour:'2-digit',
+    minute:'2-digit',
+    second:'2-digit'
+};
+element.textContent=time.toLocaleTimeString('en-US', option)
+}, 1000);
